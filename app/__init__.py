98b95d214324b6a3054ef093db9dc09ee666194e
Gabriel Silvério <gabriel.v.silverio@gmail.com>
2022-02-07 15:24:42 -0300

    Versao Estabilizada
    Versao Estabilizada do site

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 #Definindo flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.controllers import default
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
