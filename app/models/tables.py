cee725a1a691bf1dce94fb01f61f9e9443d7009a
Gabriel Silvério <gabriel.v.silverio@gmail.com>
2022-02-11 23:12:28 -0300

    Organizando Templates
    * Templates agora erdam de um template base

--- app/models/tables.py
+++ app/models/tables.py
@@ -1,21 +1,18 @@
-from sqlalchemy import ForeignKey
 from app import db
 
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    nome = db.Column(db.String)
     username = db.Column(db.String)
+    password = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
     time = db.Column(db.DateTime)
 
-    def __init__(self, username, email, password, name, time):
-        self.email = email
-        self.password = password
-        self.name = name
+    def __init__(self, email, username, password, name, time):
         self.username = username
+        self.password = password
+        self.email = email
         self.time = time
     
     def __repr__(self):
@@ -27,6 +24,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime)
 
     user = db.relationship(User, foreign_keys=user_id)
 
